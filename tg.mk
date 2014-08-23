78143ed488b480830bea1f2237e0090726732bef
k2wlxda <k2wlxda@gmail.com>
2014-08-23 11:26:17 +0530

    fix typos

--- tg.mk
+++ tg.mk
@@ -1,12 +1,17 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/tg/config/common_full_phone.mk)
+# Inherit some common Gummy stuff.
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
+
+$(call inherit-product, device/lge/w7/full_w7.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
+# Enable Torch
+PRODUCT_PACKAGES += Torch
+
 # Release name
 PRODUCT_RELEASE_NAME := LG L90
 PRODUCT_NAME := tg_w7
 
-$(call inherit-product, device/lge/w7/full_w7.mk)
+
